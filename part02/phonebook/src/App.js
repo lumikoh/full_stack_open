ec8e54a675e2e1dbd04521687833a317138bb4b8
lumi <lumineula@gmail.com>
2023-06-08 17:11:05 +0300

    exercise 2.8

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -2,39 +2,55 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   function isNameKnown(person) {
     return person.name === newName
   }
 
-  const addName = (event) => {
+  const addNumber = (event) => {
     event.preventDefault()
+    if(newName === '') return
+    
     if(persons.find(isNameKnown) !== undefined) {
       alert(`${newName} is already added to phonebook`)
       return
     }
 
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
 
   }
 
-  const handleFieldChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input 
+      <form onSubmit={addNumber}>
+        <div> name: 
+          <input 
           value={newName}
-          onChange={handleFieldChange}
-          />
+          onChange={handleNameChange} />
+        </div>
+        <div>number: 
+          <input 
+          value={newNumber}
+          onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +59,7 @@ const App = () => {
       <h2>Numbers</h2>
       <table>
         <tbody>
-        {persons.map(person => <tr key={person.name}><td>{person.name}</td></tr>)}
+        {persons.map(person => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
         </tbody>  
       </table>
     </div>
