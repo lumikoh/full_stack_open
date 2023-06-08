45d561a09d10bf14272b97da3661d941f952ceb7
lumi <lumineula@gmail.com>
2023-06-08 16:46:48 +0300

    exercise 2.6

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -6,19 +6,37 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+
+  }
+
+  const handleFieldChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleFieldChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <table>
+        <tbody>
+        {persons.map(person => <tr key={person.name}><td>{person.name}</td></tr>)}
+        </tbody>  
+      </table>
     </div>
   )
 }
