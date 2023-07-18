93d1b6383585058b9b9b45f44b73c416e8638849
lumi <lumineula@gmail.com>
2023-07-18 16:41:03 +0300

    exercise 3.6 and fix to 3.5

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -53,11 +53,7 @@ app.delete("/api/persons/:id", (request, response) => {
 })
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-
-  return maxId + 1
+  return Math.floor(Math.random()*100000)
 }
 
 
@@ -66,6 +62,12 @@ app.post("/api/persons", (request, response) => {
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'content missing'
+    })
+  }
+
+  if(persons.find( person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
 
