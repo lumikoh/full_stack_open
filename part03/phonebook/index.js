b44694b43eefe59331e83381c2a5650b00cf6e30
lumi <lumineula@gmail.com>
2023-07-18 15:33:59 +0300

    exercise 3.2

--- part03/phonebook/index.js
+++ part03/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 let persons = [
     { 
       "id": 1,
@@ -30,6 +31,11 @@ app.get( '/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>` +
+                  `<p>${new Date()}</p>`)
 })
 
 const PORT = 3001
