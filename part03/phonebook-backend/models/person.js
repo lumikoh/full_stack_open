98e5d315431cf480a2dcb9bd6c15c10d9e3474f8
lumi <lumineula@gmail.com>
2023-07-19 23:04:17 +0300

    exercise 3.20

--- part03/phonebook-backend/models/person.js
+++ part03/phonebook-backend/models/person.js
@@ -22,7 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minLength: 8,
+    validate: {
+        validator: v => {
+            return /^\d{2,3}-\d+/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true,
   },
 })
 
