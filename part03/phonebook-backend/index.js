3d3ddf56118e87e4b6e998cb44097e1023ef1a34
lumi <lumineula@gmail.com>
2023-07-19 22:01:57 +0300

    a small change to a crash

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -86,12 +86,6 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
   const person = new Person({
     name: body.name,
     number: body.number,
