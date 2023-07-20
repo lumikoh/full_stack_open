7d961e4b8fc55f977b3903897359fb19b0263cdd
lumi <lumineula@gmail.com>
2023-07-20 14:53:26 +0300

    exercise 4.4

--- part04/bloglist/utils/list_helper.js
+++ part04/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogList) => {
+  return blogList.reduce( (sum, item) => {
+    return sum + item.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
