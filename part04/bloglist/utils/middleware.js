df3413f36f30610d7199745a61b78997ac9b5fb1
lumi <lumineula@gmail.com>
2023-07-20 14:08:15 +0300

    exercise 4.2

---
+++ part04/bloglist/utils/middleware.js
@@ -0,0 +1,32 @@
+const logger = require('./logger')
+const morgan = require('morgan')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+const requestLogger = morgan(
+  ':method :url :status :res[content-length] - :response-time ms :content'
+)
+
+morgan.token('content', (request) => {
+  return JSON.stringify(request.body)
+})
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
