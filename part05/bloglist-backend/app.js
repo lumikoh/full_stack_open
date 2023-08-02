67ac04aa4edf03ff5057f2a7decd62264b5347f2
lumi <lumineula@gmail.com>
2023-08-03 01:21:24 +0300

    exercise 5.17

--- part05/bloglist-backend/app.js
+++ part05/bloglist-backend/app.js
@@ -9,14 +9,14 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
-
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 mongoose.set('strictQuery', false)
 
 logger.info('Connecting to MongoDB...')
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('Connected to MongoDB')
   })
@@ -33,6 +33,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs/', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
