101e77d9e2f9b8ea7bbce8db03825f4bdbc6cd13
lumi <lumineula@gmail.com>
2023-08-02 18:38:00 +0300

    added the backend from the previous part under part05

---
+++ part05/bloglist-backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const config = require('./utils/config')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+
+const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to MongoDB...')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs/', blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part05/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,75 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const middleware = require('../utils/middleware')
+
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
+})
+
+blogRouter.post('/', middleware.userExtractor, async (request, response) => {
+
+  const userId = request.user
+
+  if(!userId) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(userId)
+
+  const blog = new Blog({
+    _id: request.body.id,
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+    __v: request.body.__v,
+    user: user.id
+  })
+
+  const result = await blog.save()
+
+  user.blogs = user.blogs.concat(result.id)
+  await user.save()
+
+  response.status(201).json(result)
+
+})
+
+blogRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+
+  const user = request.user
+
+  if(!user) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const deleted = await Blog.findById(request.params.id)
+
+  if(deleted.user.toString() !== user.toString()) {
+    return response.status(401).json({ error: 'invalid user' })
+  }
+
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+
+  const blog = new Blog({
+    _id: request.params.id,
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+  })
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+
+})
+
+module.exports = blogRouter
