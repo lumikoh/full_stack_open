6a9ce134509f93f2ceab78fcb23fb022f436b8da
lumi <lumineula@gmail.com>
2023-08-02 15:15:00 +0300

    exercise 5.7

--- part05/bloglist-frontend/src/components/Blog.js
+++ part05/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+      </div>
+      <div style={showWhenVisible}>
+       {blog.url}
+       <br></br>
+       {"likes " + blog.likes}
+       <button>like</button>
+       <br></br>
+       {blog.user.name}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
