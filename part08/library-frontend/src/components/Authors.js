aaf0f2e42a5b97195f430c9168a1c15a2399e033
lumi <lumineula@gmail.com>
2023-08-17 17:35:02 +0300

    exercise 8.11

--- part08/library-frontend/src/components/Authors.js
+++ part08/library-frontend/src/components/Authors.js
@@ -1,9 +1,36 @@
+import { gql, useMutation } from "@apollo/client"
+import { useState } from "react"
+
+const UPDATE_BIRTHYEAR = gql`
+mutation updateBirth($name: String!, $setBornTo: Int!){
+  editAuthor(name: $name, setBornTo: $setBornTo) {
+    name
+    born
+  }
+}
+`
+
 const Authors = ({show, query}) => {
+  const [name, setName] = useState('')
+  const [birth, setBirth] =useState('')
+
+  const [ updateBirth ] = useMutation(UPDATE_BIRTHYEAR)
+
   if (!show || !query.data) {
     return null
   }
  
   const authors = query.data.allAuthors
+
+  const submit = event => {
+    event.preventDefault()
+
+    updateBirth({ variables: {name, setBornTo: Number(birth)}})
+
+    setName('')
+    setBirth('')
+
+  }
 
   return (
     <div>
@@ -24,6 +51,16 @@ const Authors = ({show, query}) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        name
+        <input value={name} onChange={ ({target}) => setName(target.value)}/>
+        <br />
+        born
+        <input value={birth} onChange={ ({target}) => setBirth(target.value)}/>
+        <br />
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
