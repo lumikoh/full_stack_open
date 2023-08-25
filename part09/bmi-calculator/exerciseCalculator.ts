c849b986aadedf2baa6cbdfeb250eb0193d2a19b
lumi <lumineula@gmail.com>
2023-08-25 18:25:34 +0300

    exercise 9.2

---
+++ part09/bmi-calculator/exerciseCalculator.ts
@@ -0,0 +1,48 @@
+interface Result { 
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number }
+
+const calculateExercises = (dayArray: number[], target: number): Result => {
+    const trainingDays = dayArray.reduce((totalDays, value) => {
+        return value > 0 ? totalDays+1 : totalDays
+    }, 0)
+
+    const average = dayArray.reduce((sum, hours ) => {
+        return sum + hours
+    }, 0) / dayArray.length
+
+    let rating = 3
+    let ratingDescription =  'good job'
+    let success = true
+
+    if(average < target*0.8) {
+        rating = 1
+        ratingDescription = 'not nearly enough time'
+        success = false
+    }
+    else if(average < target) {
+        rating = 2
+        ratingDescription = 'could be better'
+        success = false
+    }
+
+
+    return {
+        periodLength: dayArray.length,
+        trainingDays,
+        success: true,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+
+}
+
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
